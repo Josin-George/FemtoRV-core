dde31604993287589062dcbfacb3de2fe27cbf1f
Michael_j_nj <georgejosin6@gmail.com>
2025-11-01 23:32:54 +0530

    instruction Decoding

--- FemtoRV.v
+++ FemtoRV.v
@@ -36,8 +36,19 @@ wire [31:0] Jimm = {{12{instr[31]}},instr[19:12],instr[20],instr[30:21],1'b0};
 
 //10 different instruction
 
-wire isLoad = (instr[6:2] == 5'b00000);//rd <- mem[rs1+Iimm]
-wire isAlU
+wire isLoad     = (instr[6:2] == 5'b00000);//rd <- mem[rs1+Iimm]
+wire isAlUimm   = (instr[6:2] == 5'b00100);//rd <- rs1 OP Iimm
+wire isStore    = (instr[6:2] == 5'b01000);//mem[rs1+Simm] <- rs2
+wire isAlUreg   = (instr[6:2] == 5'b01100);//rd <- rs1 OP rs2
+wire isSYSTEM   = (instr[6:2] == 5'b11100);//rd <- cycles
+wire isJAL      = (instr[3])//instr[6:2] == 5'b11011);//rd <- PC + 4; PC <- PC + Jimm
+wire isJALR     = (instr[6:2] == 5'b11001);//rd <- PC + 4; PC<- PC+Jimm
+wire isLUI      = (instr[6:2] == 5'b01101);//rd <- Uimm
+wire isAUIPC    = (instr[6:2] == 5'b00101);//rd <- PC + Uimm
+wire Branch     = (instr[6:2] == 5'b11000);//if(rs1 OP rs2) PC<-PC + Bimm
+
+wire isALU = isALUimm | isALUreg;
+
 
 
 //register file 
@@ -52,3 +63,45 @@ begin
         if(rdId !=0)
             registerFile[rdId] <= writeBackData;
 end
+
+//ALU 
+wire [31:0] aluIn1 = rs1;
+
+wire [31:0] aluInt2 = isALUreg | isBranch ? rs2 : Iimm;
+
+reg [31:0] aluReg;//The internal register of the ALU used by shift 
+reg [4:0] aluShamt;//current shift amount
+
+wire aluBusy = | aluShamt;
+wire aluWr;
+
+wire [31:0] aluPlus = aluIn1 + aluIn2;
+
+wire [32:0] aluMinus = {1'b1, ~aluIn2} + {1'b0,aluIn1} + 33'b1;
+wire LT = (aluIn1[31] ^ aluIn2[31]) ? aluIn1[31] : aluMinus[32];
+wire LTU = aluMinus[32];
+wire EQ = (aluMinus[31:0] == 0);
+
+wire [31:0] auOut = 
+(funct3Is[0] ? instr[30] & instr[5] ? aluMinus[31:0] : aluPlus  : 32'b0) |
+(funct3Is[2] ? {31'b0, LT}                                      : 32'b0) |
+(funct3Is[3] ? {31'b0, LTU}                                     : 32'b0) |
+(funct3Is[4] ? aluIn1 ^ aluIn2                                  : 32'b0) |
+(funct3Is[6] ? aluIn1 | aluIn2                                  : 32'b0) |
+(funct3Is[7] ? aluIn1 & aluIn2                                  : 32'b0) |
+(funct3IsShift ? aluReg                                         : 32'b0);
+
+wire funct3IsShift = funct3Is[1] | funct3Is[5];
+
+always@(posedge clk) 
+begin
+    if(aluWr)
+        begin
+            if(funct3IsShift)
+            begin
+                aluReg <= aluIn1;//SLL, SRA, SRl
+                aluShamt <= aluInt2[4:0];
+            end
+        end
+end
+
